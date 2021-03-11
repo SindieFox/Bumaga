2dc6cdd9dd1b06d9e8fefd469a1eb7e4a8af3c81
Liska Sid <sid427@gmail.com>
2021-03-11 21:49:48 +0300

    first commit

---
+++ CCC/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿
+namespace CCC {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.HideSelection = false;
+            this.textBox1.Name = "textBox1";
+            // 
+            // comboBox1
+            // 
+            resources.ApplyResources(this.comboBox1, "comboBox1");
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            resources.GetString("comboBox1.Items"),
+            resources.GetString("comboBox1.Items1"),
+            resources.GetString("comboBox1.Items2"),
+            resources.GetString("comboBox1.Items3"),
+            resources.GetString("comboBox1.Items4"),
+            resources.GetString("comboBox1.Items5"),
+            resources.GetString("comboBox1.Items6")});
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ CCC/Form1.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CCC {
+    public partial class Form1 : Form {
+        private Color[] colorBG = { Color.White, Color.LightBlue, Color.LightCoral, Color.PaleGreen, Color.Gainsboro, Color.LemonChiffon, Color.Thistle };
+        private RegistryKey key = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\Bumaga");
+
+        public Form1() {
+            InitializeComponent();
+
+            this.Location = (key.GetValue("PosX") == null) ? new Point(200, 200) : new Point(Convert.ToInt32(key.GetValue("PosX")), Convert.ToInt32(key.GetValue("posY")));
+
+            if (key.GetValue("SizeWidht") != null)
+                this.Size = new Size(Convert.ToInt32(key.GetValue("SizeWidht")), Convert.ToInt32(key.GetValue("SizeHeight")));
+            
+            this.textBox1.Text = Convert.ToString(key.GetValue("Text"));
+            this.textBox1.BackColor = colorBG[Convert.ToInt32(key.GetValue("Color"))];
+
+            this.comboBox1.SelectionChangeCommitted += new EventHandler(this.СolorSelection);
+            this.FormClosing += Form1_FormClosing;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) {
+            key.SetValue("SizeWidht", this.Width);
+            key.SetValue("SizeHeight", this.Height);
+            key.SetValue("PosX", this.Location.X);
+            key.SetValue("PosY", this.Location.Y);
+            key.SetValue("Text", this.textBox1.Text);
+        }
+
+        private void СolorSelection(object sender, EventArgs e) {
+            this.textBox1.BackColor = colorBG[this.comboBox1.SelectedIndex];
+            key.SetValue("Color", this.comboBox1.SelectedIndex);
+        }
+
+    }
+}
